60a4887bc4aa8677b83bd5cb15cc4d7bc7f6cd1a
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-05 17:14:14 +0700

    Fix formatting issue in toRupiah() function

--- index.test.ts
+++ index.test.ts
@@ -4,9 +4,9 @@ import { toRupiah } from ".";
 describe("Number utility functions", () => {
   describe("toRupiah()", () => {
     describe.each([
-      [1000, "Rp1.000"],
-      [15000, "Rp15.000"],
-      [1234567890, "Rp1.234.567.890"],
+      [1000, "Rp 1.000"],
+      [15000, "Rp 15.000"],
+      [1234567890, "Rp 1.234.567.890"],
     ])("toRupiah() - %i should format to %s", (input, expected) => {
       it(`formats ${input} into Indonesian Rupiah as ${expected}`, () => {
         expect(toRupiah(input)).toEqual(expected);
@@ -14,9 +14,9 @@ describe("Number utility functions", () => {
     });
 
     describe.each([
-      [0, "Rp0"],
-      [-1000, "-Rp1.000"],
-      [-15000, "-Rp15.000"],
+      [0, "Rp 0"],
+      [-1000, "-Rp 1.000"],
+      [-15000, "-Rp 15.000"],
       [-1234567890, "-Rp1.234.567.890"],
     ])(
       "toRupiah() - %i should handle zero and negative correctly as %s",
@@ -31,6 +31,7 @@ describe("Number utility functions", () => {
       expect(() => toRupiah(NaN)).toThrow(
         "Invalid input: The input should be a valid number."
       );
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument
       expect(() => toRupiah(undefined as any)).toThrow(
         "Invalid input: The input should be a valid number."
       );
