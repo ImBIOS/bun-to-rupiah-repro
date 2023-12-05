ff4addc50aee41545dd3182c1caf20937a674774
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-05 17:46:40 +0700

    Add tests for formatting numbers, dates, lists, plural rules, relative time, and display names in multiple locales and currencies

--- web-intl.test.ts
+++ web-intl.test.ts
@@ -1,5 +1,40 @@
 import { describe, expect, it } from "bun:test";
 
+// Define a set of diverse locales and currencies
+const locales = [
+  "id-ID",
+  "en-US",
+  "fr-FR",
+  "ja-JP",
+  "de-DE",
+  "es-ES",
+  "it-IT",
+  "pt-BR",
+  "ru-RU",
+  "tr-TR",
+  "zh-CN",
+  "ar-SA",
+  "hi-IN",
+  "ar-KW",
+  "ar-BH",
+];
+const currencies = [
+  "IDR",
+  "USD",
+  "EUR",
+  "JPY",
+  "GBP",
+  "MXN",
+  "BRL",
+  "RUB",
+  "TRY",
+  "CNY",
+  "SAR",
+  "INR",
+  "KWD",
+  "BHD",
+];
+
 describe("Web Intl", () => {
   it("has globals", () => {
     expect(Intl.Collator.name).toBe("Collator");
@@ -12,68 +47,53 @@ describe("Web Intl", () => {
     expect(Intl.RelativeTimeFormat.name).toBe("RelativeTimeFormat");
   });
 
-  it("should format", () => {
-    const date = new Date("2021-01-01T00:00:00.000Z");
-    const number = 123456.789;
-    const locale = "en-US";
-    const options = { style: "currency", currency: "USD" };
-    expect(new Intl.DateTimeFormat(locale).format(date)).toBe("1/1/2021");
-    expect(new Intl.NumberFormat(locale, options).format(number)).toBe(
-      "$123,456.79"
-    );
-  });
+  // Test for multiple locales and currencies
+  locales.forEach((locale) => {
+    currencies.forEach((currency) => {
+      it(`should format number as currency correctly in ${locale} with ${currency}`, () => {
+        const number = 123456.789;
+        const options = { style: "currency", currency };
+        expect(
+          new Intl.NumberFormat(locale, options).format(number)
+        ).toMatchSnapshot();
+      });
 
-  it("should list", () => {
-    const list = ["a", "b", "c"];
-    expect(new Intl.ListFormat("en-US").format(list)).toBe("a, b, and c");
-  });
+      it(`should format date correctly in ${locale}`, () => {
+        const date = new Date("2021-01-01T00:00:00.000Z");
+        expect(new Intl.DateTimeFormat(locale).format(date)).toMatchSnapshot();
+      });
 
-  it("should pluralize", () => {
-    const number = 1;
-    expect(new Intl.PluralRules("en-US").select(number)).toBe("one");
-  });
+      it(`should format list correctly in ${locale}`, () => {
+        const list = ["a", "b", "c"];
+        expect(new Intl.ListFormat(locale).format(list)).toMatchSnapshot();
+      });
 
-  it("should format relative time", () => {
-    const number = -1;
-    const unit = "day";
-    expect(new Intl.RelativeTimeFormat("en-US").format(number, unit)).toBe(
-      "1 day ago"
-    );
-  });
+      it(`should format plural rules correctly in ${locale}`, () => {
+        const number = 1;
+        expect(new Intl.PluralRules(locale).select(number)).toMatchSnapshot();
+      });
 
-  it("should format display names", () => {
-    const locale = "en-US";
-    const options: Intl.DisplayNamesOptions = { type: "language" };
-    expect(new Intl.DisplayNames(locale, options).of("en")).toBe("English");
-  });
+      it(`should format relative time correctly in ${locale}`, () => {
+        const number = -1;
+        const unit = "day";
+        expect(
+          new Intl.RelativeTimeFormat(locale).format(number, unit)
+        ).toMatchSnapshot();
+      });
 
-  it("should compare", () => {
-    const collator = new Intl.Collator("en-US");
-    const a = "a";
-    const b = "b";
-    expect(collator.compare(a, b)).toBe(-1);
-  });
+      it(`should format display names correctly in ${locale}`, () => {
+        const options: Intl.DisplayNamesOptions = { type: "language" };
+        expect(
+          new Intl.DisplayNames(locale, options).of("en")
+        ).toMatchSnapshot();
+      });
 
-  it("should format numbers", () => {
-    const number = 123456.789;
-    expect(new Intl.NumberFormat("en-US").format(number)).toBe("123,456.789");
-  });
-
-  it("should format dates", () => {
-    const date = new Date("2021-01-01T00:00:00.000Z");
-    expect(new Intl.DateTimeFormat("en-US").format(date)).toBe("1/1/2021");
-  });
-
-  it("should format relative time", () => {
-    const number = -1;
-    const unit = "day";
-    expect(new Intl.RelativeTimeFormat("en-US").format(number, unit)).toBe(
-      "1 day ago"
-    );
-  });
-
-  it("should format lists", () => {
-    const list = ["a", "b", "c"];
-    expect(new Intl.ListFormat("en-US").format(list)).toBe("a, b, and c");
+      it(`should compare strings correctly in ${locale}`, () => {
+        const collator = new Intl.Collator(locale);
+        const a = "a";
+        const b = "b";
+        expect(collator.compare(a, b)).toMatchSnapshot();
+      });
+    });
   });
 });
